582e94d0758c32883d2ffa108d24315b4cf2befb
wangjue <wangjue@startdt.com>
2018-09-19 15:37:49 +0800

    add map

--- src/ivew/layout.vue
+++ src/ivew/layout.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="layout">
-    <div class="nav"></div>
+    <div class="nav">
+      <div class="nav-left">
+        <span>2017年08年09日</span>
+        <span>星期三</span>
+      </div>
+      <div class="nav-mid">
+        监控数据威胁情报处置平台
+      </div>
+      <div class="nav-right">
+        关于“监控数据威胁情报处置平台”的相关报道
+      </div>
+    </div>
     <div class="container">
       <div class="container-left">
         <layoutLeft></layoutLeft>
@@ -8,7 +19,9 @@
       <div class="container-right">
         <layoutRight></layoutRight>
       </div>
-      <div class="container-flex"></div>
+      <div class="container-flex">
+        <maps></maps>
+      </div>
     </div>
   </div>
 </template>
@@ -16,10 +29,12 @@
 <script>
 import layoutLeft from "@/components/layoutLeft/layoutLeft";
 import layoutRight from "@/components/layoutRight/layoutRight";
+import maps from "@/components/map/map";
 export default {
   components: {
     layoutLeft,
-    layoutRight
+    layoutRight,
+    maps
   }
 };
 </script>
@@ -28,7 +43,27 @@ export default {
 .nav {
   width: 100%;
   height: 40px;
-  background-color: pink;
+  background-color: #141d2c;
+  display: flex;
+  color: #fff;
+  font-size: 10px;
+  &-left {
+    width: 260px;
+    margin-top: 20px;
+    margin-left: 10px;
+  }
+  &-mid {
+    flex: 1;
+    color: #5dc3c6;
+    line-height: 40px;
+    font-size: 20px;
+    text-align: center;
+  }
+  &-right {
+    width: 260px;
+    margin-top: 20px;
+    text-align: right;
+  }
 }
 .container {
   display: flex;
@@ -43,10 +78,12 @@ export default {
   }
   &-flex {
     position: absolute;
-    height: 80%;
-    width: 40%;
+    height: 500px;
+    width: 500px;
+    // height: 80%;
+    // width: 40%;
     background-color: salmon;
-    left: 36%;
+    left: 26%;
   }
 }
 </style>
